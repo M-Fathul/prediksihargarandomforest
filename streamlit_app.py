0a20eb8ba92b50e4d5e96186dc6c5308e7ab38fb
Muhammad Fathul Mubiin <mfathulmubiin@gmail.com>
2026-08-30 23:37:25 +0700

    fear:remove side bar

--- streamlit_app.py
+++ streamlit_app.py
@@ -190,45 +190,45 @@ with spek2:
   jumlahfueltype = df['fuelType'].value_counts()
   st.bar_chart(jumlahfueltype)
 
-with st.sidebar:
-  if 'Make' not in st.session_state:
-    st.session_state.Make = df['Make'].unique()[0]
-  if 'model' not in st.session_state:
-    st.session_state.model = df[df['Make'] == st.session_state.Make]['model'].unique()[0]
-  st.session_state.Make = st.selectbox('Make', df['Make'].unique(), key='Make_select')
-  filtered_models = df[df['Make'] == st.session_state.Make]['model'].unique()
-  st.session_state.model = st.selectbox('Model', filtered_models, key='model_select')
-  year = st.number_input('Tahun Beli', df['year'].min(), df['year'].max(), 2017)
-  transmission = st.selectbox('Transmisi', df['transmission'].unique())
-  fuelType = st.selectbox('Bahan Bakar', df['fuelType'].unique())
-  engineSize = st.number_input('Ukuran Mesin', df['engineSize'].min(), df['engineSize'].max(), 1.4)
-  mileage = st.number_input('Jarak Tempuh', df['mileage'].min(), df['mileage'].max(), 15735)
-  mpg = st.number_input('Kapasitas Bahan Bakar', df['mpg'].min(), df['mpg'].max(), 55.4)
-  tax = st.number_input('Pajak', df['tax'].min(), df['tax'].max(), 150.0)
-  price = 0
-  prediksi = 0
-  if st.button('prediksi harga'):
-    new_data = pd.DataFrame({
-      'model': [st.session_state.model],
-      'year': [year],
-      'price': [price],
-      'transmission': [transmission],
-      'mileage': [mileage],
-      'fuelType': [fuelType],
-      'tax': [tax],
-      'mpg': [mpg],
-      'engineSize': [engineSize],
-      'Make': [st.session_state.Make],
-    })
-    new_data_prep = new_data.copy()
-    numerical_features = new_data_prep.select_dtypes(exclude=['object']).columns
-    new_data_prep[numerical_features] = scaler.transform(new_data_prep[numerical_features])
-    for col in new_data_prep.select_dtypes(include=['object']):
-      new_data_prep[col] = labeling.transform(new_data_prep[col])
-    new_data_prep = new_data_prep.drop('price', axis=1)
-    y_pred_scaled = modelRandomForest.predict(new_data_prep)
-    new_data_prep.insert(2, 'price', y_pred_scaled)
-    numerical_features = new_data.select_dtypes(exclude=['object']).columns
-    new_data[numerical_features] = scaler.inverse_transform(new_data_prep[numerical_features])
-    prediksi = int(new_data['price'])
-    st.write('Prediksi Harga Mobil Bekas: ', + prediksi)
+# with st.sidebar:
+#   if 'Make' not in st.session_state:
+#     st.session_state.Make = df['Make'].unique()[0]
+#   if 'model' not in st.session_state:
+#     st.session_state.model = df[df['Make'] == st.session_state.Make]['model'].unique()[0]
+#   st.session_state.Make = st.selectbox('Make', df['Make'].unique(), key='Make_select')
+#   filtered_models = df[df['Make'] == st.session_state.Make]['model'].unique()
+#   st.session_state.model = st.selectbox('Model', filtered_models, key='model_select')
+#   year = st.number_input('Tahun Beli', df['year'].min(), df['year'].max(), 2017)
+#   transmission = st.selectbox('Transmisi', df['transmission'].unique())
+#   fuelType = st.selectbox('Bahan Bakar', df['fuelType'].unique())
+#   engineSize = st.number_input('Ukuran Mesin', df['engineSize'].min(), df['engineSize'].max(), 1.4)
+#   mileage = st.number_input('Jarak Tempuh', df['mileage'].min(), df['mileage'].max(), 15735)
+#   mpg = st.number_input('Kapasitas Bahan Bakar', df['mpg'].min(), df['mpg'].max(), 55.4)
+#   tax = st.number_input('Pajak', df['tax'].min(), df['tax'].max(), 150.0)
+#   price = 0
+#   prediksi = 0
+#   if st.button('prediksi harga'):
+#     new_data = pd.DataFrame({
+#       'model': [st.session_state.model],
+#       'year': [year],
+#       'price': [price],
+#       'transmission': [transmission],
+#       'mileage': [mileage],
+#       'fuelType': [fuelType],
+#       'tax': [tax],
+#       'mpg': [mpg],
+#       'engineSize': [engineSize],
+#       'Make': [st.session_state.Make],
+#     })
+#     new_data_prep = new_data.copy()
+#     numerical_features = new_data_prep.select_dtypes(exclude=['object']).columns
+#     new_data_prep[numerical_features] = scaler.transform(new_data_prep[numerical_features])
+#     for col in new_data_prep.select_dtypes(include=['object']):
+#       new_data_prep[col] = labeling.transform(new_data_prep[col])
+#     new_data_prep = new_data_prep.drop('price', axis=1)
+#     y_pred_scaled = modelRandomForest.predict(new_data_prep)
+#     new_data_prep.insert(2, 'price', y_pred_scaled)
+#     numerical_features = new_data.select_dtypes(exclude=['object']).columns
+#     new_data[numerical_features] = scaler.inverse_transform(new_data_prep[numerical_features])
+#     prediksi = int(new_data['price'])
+#     st.write('Prediksi Harga Mobil Bekas: ', + prediksi)
